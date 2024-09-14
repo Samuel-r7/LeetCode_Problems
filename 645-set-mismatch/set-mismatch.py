243221c0826be4d1f30381462f6eec16a9b02bef
Samuel-r7 <164600891+Samuel-r7@users.noreply.github.com>
2024-09-14 12:59:06 +0530

    Time: 166 ms (39.08%) | Memory: 18 MB (64.50%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -5,12 +5,12 @@ class Solution:
         for num in range(len(nums)-1):
             if nums[num] == nums[num+1]:
                 res[0] = nums[num]  
-                print(res[0])
+                # print(res[0])
+        
+        n=len(nums)
+        expected_sum = (n*(n+1))//2
 
-        expected_sum = sum(range(1, len(nums) + 1))
-        print(expected_sum)
         actual_sum = sum(nums)
-        print(actual_sum)
         res[1] = expected_sum - (actual_sum - res[0]) 
 
         return res
