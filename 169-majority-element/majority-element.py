351b669aae9de4ce3280980c56bde59fa211dd75
Samuel-r7 <164600891+Samuel-r7@users.noreply.github.com>
2024-09-27 14:15:28 +0530

    Time: 172 ms (50.65%) | Memory: 18 MB (24.65%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,5 +4,15 @@ class Solution:
         # freq = Counter(nums)
         # return freq.most_common(1)[0][0]
         
-        nums.sort()
-        return nums[len(nums)//2]
+        # nums.sort()
+        # return nums[len(nums)//2]
+
+        freq ={}
+
+        for num in nums:
+            if num in freq:
+                freq[num] +=1
+            else:
+                freq[num] = 1
+        maj = max(freq, key = freq.get)
+        return maj
