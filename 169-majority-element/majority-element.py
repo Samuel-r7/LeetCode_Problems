563fe72140b8526fae60da18b9f48bf451e6c9d1
Samuel-r7 <164600891+Samuel-r7@users.noreply.github.com>
2024-09-27 14:11:54 +0530

    Time: 170 ms (60.33%) | Memory: 18 MB (81.28%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        freq = Counter(nums)
-        return freq.most_common(1)[0][0]
+        # freq = Counter(nums)
+        # return freq.most_common(1)[0][0]
+        
+        nums.sort()
+        return nums[len(nums)//2]
